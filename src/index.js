6a0d054b86661effb9cf4b4202043faca91583b6
miyan1221 <103588276+miyan1221@users.noreply.github.com>
2023-06-06 19:22:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,8 +6,8 @@ import fetch from 'node-fetch';
 
 const port = parseInt(process.env.PORT || '8080', 10);
 const api_keys = JSON.parse(process.env.API_KEYS);
-const upstreamUrl = 'https://api.openai.com/v1/chat/completions';
-
+const upstreamUrl = 'https://chatgpt-api.shn.hk/v1/chat/completions';
+///https://api.openai.com/v1
 const corsHeaders = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Methods': 'GET, HEAD, POST, OPTIONS',
